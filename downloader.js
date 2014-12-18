79124c8a654f54cd731e6c11115f147d25c3e101
CyberLight <cyberlight@live.ru>
2014-12-18 23:38:17 +0600

    Downloader: Added functionality for download subtitles and videos inside nested folders path /SERIES/SEASON/EPISODE.mp4 or webm    * Added post data request to special api

--- downloader.js
+++ downloader.js
@@ -4,7 +4,7 @@ var async = require('async');
 var url = require('url');
 var http = require('http');
 var util = require('util');
-var fs = require('fs');
+var request = require('request');
 var EventEmitter = require('events').EventEmitter;
 var mkdirp = require('mkdirp');
 var USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36';
@@ -16,6 +16,9 @@ if(process.argv.length == 2){
 
 var jsonDataFile = process.argv[2];
 var pathToSaveFiles = process.argv[3];
+var canPostData = (process.argv[4] == 'post-data');
+var host = process.argv[5];
+var seriesId = process.argv[6];
 
 console.reset = function () {
     return process.stdout.write('\033c');
@@ -78,9 +81,10 @@ function downloadFile(src, output, headers) {
 }
 
 
-function download_using_node_wget(fileUrl, outputdir, headers, cb) {
-    var fileName = url.parse(fileUrl).pathname.split('/').pop();
-    var download = downloadFile(fileUrl, outputdir + '/' + fileName, headers);
+function download_using_node_wget(fileUrl, fileName, outputdir, headers, cb) {
+    fileName = fileName || url.parse(fileUrl).pathname.split('/').pop();
+    var filePathWithName = outputdir + '/' + fileName;
+    var download = downloadFile(fileUrl, filePathWithName, headers);
     download.on('error', function (err) {
         cb(err, null);
     });
@@ -90,7 +94,7 @@ function download_using_node_wget(fileUrl, outputdir, headers, cb) {
     download.on('progress', function (progress) {
         var currentProgress = progress * 100;
         console.reset();
-        console.log('fileName: ', fileName, ' progress: ', currentProgress, '%');
+        console.log('fileName: ', filePathWithName, ' progress: ', currentProgress, '%');
     });
 }
 
@@ -102,11 +106,15 @@ function downloadSubtitles(subtitles, threads, cb) {
 
     async.eachLimit(subtitles, threads, function (url, next) {
         console.log('download: ', url);
-        mkdirp(pathToSaveFiles, function(err) {
+        var fullPathToSaveFiles = pathToSaveFiles + url.filePath;
+        mkdirp(fullPathToSaveFiles, function(err) {
             if(err){
                 throw err;
             }
-            download_using_node_wget(url, pathToSaveFiles, {'User-Agent': USER_AGENT}, next);
+            download_using_node_wget(url.src,
+                                     null,
+                                     fullPathToSaveFiles, {'User-Agent': USER_AGENT},
+                                     next);
         });
     }, function (err) {
         if (err) {
@@ -127,11 +135,43 @@ function downloadVideos(videos, threads, cb) {
 
     async.eachLimit(videos, threads, function (url, next) {
         console.log('download: ', url);
-        mkdirp(pathToSaveFiles, function(err) {
+        var fullPathToSave = pathToSaveFiles + url.filePath;
+        mkdirp(fullPathToSave, function(err) {
             if (err) {
                 throw err;
             }
-            download_using_node_wget(url, pathToSaveFiles, {'Cookie': 'video=true', 'User-Agent': USER_AGENT}, next);
+
+            var headers = {'Cookie': 'video=true', 'User-Agent': USER_AGENT};
+            download_using_node_wget(url.src,
+                                     url.fileName,
+                                     fullPathToSave,
+                                     headers,
+
+                function(err, data){
+                    if(canPostData && !err) {
+
+                        var formData = {
+                            title: url.title,
+                            number: url.episodeNumber,
+                            description: url.description,
+                            published: 'True'
+                        };
+
+                        postData(host,
+                                 url.seriesId,
+                                 url.seasonNumber,
+                                 formData,
+                            function(err, data){
+                                if(err)
+                                    console.log("Error posting data to API: ", err, " for ", url.src);
+
+                                next(err, data);
+                            });
+
+                    }else {
+                        next(err, data);
+                    }
+                });
         });
     }, function (err) {
         if (err) {
@@ -144,6 +184,26 @@ function downloadVideos(videos, threads, cb) {
     });
 }
 
+function postData(host, seriesId, seasonNumber, formData, cb){
+    request.post(
+        host + '/api/series/' + seriesId + '/season/'+ seasonNumber +'/create_episode',
+        {
+            form: formData
+        },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                cb(null, true);
+            } else {
+                if(error) {
+                    cb(error, false);
+                }else if(response.statusCode != 200){
+                    cb(new Error("Status code " + response.statusCode));
+                }
+            }
+        }
+    );
+}
+
 fs.readFile(jsonDataFile, 'utf8', function (err, data) {
     if (err) throw err;
     var downloadData = JSON.parse(data);
@@ -153,11 +213,31 @@ fs.readFile(jsonDataFile, 'utf8', function (err, data) {
 
     for(var i=0, len=downloadData.videoInfos.length; i<len; i++) {
         var videoInfo = downloadData.videoInfos[i];
+        var title = videoInfo.title;
+        var parsedTitle = /Season\s(.*)\sEpisode\s(.*)/.exec(title);
+        var seasonNumber = parsedTitle && parsedTitle[1] || 1;
+        var episodeNumber = parsedTitle && parsedTitle[2] || 1;
+
         for(var j=0, jlen=videoInfo.sourceUrls.length; j<jlen; j++) {
-            videos.push(videoInfo.sourceUrls[j].src);
+            var sourceUrl = videoInfo.sourceUrls[j];
+            var fileExtension = sourceUrl.src.split('.').pop();
+
+            videos.push({
+                title: title,
+                seriesId: seriesId,
+                seasonNumber: seasonNumber,
+                episodeNumber: episodeNumber,
+                filePath: '/'+seriesId+'/'+seasonNumber,
+                fileName: episodeNumber+'.'+fileExtension,
+                description: downloadData.description,
+                src: sourceUrl.src
+            });
         }
         for(var k=0, klen=videoInfo.trackUrls.length; k<klen; k++) {
-            subtitles.push(videoInfo.trackUrls[k].src);
+            subtitles.push({
+                filePath: '/'+seriesId+'/'+seasonNumber,
+                src: videoInfo.trackUrls[k].src
+            });
         }
     }
 
@@ -180,6 +260,5 @@ fs.readFile(jsonDataFile, 'utf8', function (err, data) {
             }
         });
     });
-
 
 });
