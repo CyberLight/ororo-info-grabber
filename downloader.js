ea5d36f04d98039bab544e66c0559c10ea6f119a
CyberLight <cyberlight@live.ru>
2015-01-06 14:54:41 +0600

    Downloader: Added support for downloading many urls: movie and series from list_XXXXXXXXXX.json file generated by grabber

--- downloader.js
+++ downloader.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var async = require('async');
 var url = require('url');
 var http = require('http');
+var path = require('path');
 var util = require('util');
 var request = require('request');
 var EventEmitter = require('events').EventEmitter;
@@ -18,8 +19,17 @@ function EpisodeAlreadyExistsError(seriesId, seasonNumber, episodeNumber){
     ' does not created or already exists!'
 }
 
+function SeriesAlreadyExistsError(seriesId){
+    this.message = 'Serial with seriesId: ' + seriesId +
+                   'already exists!'
+}
+
 function FileExistsError(filePath){
     this.message = 'File ' + filePath + ' already exists!';
+}
+
+function SkippedSeriesError(path, errMsg){
+    this.message = 'File with series metadata ' + path + ' skipped! Err: ' + JSON.stringify(errMsg);
 }
 
 EpisodeAlreadyExistsError.prototype = new Error();
@@ -31,16 +41,27 @@ FileExistsError.prototype.coloredMessage = function(){
     return this.message.yellow;
 };
 
+SeriesAlreadyExistsError.prototype = new Error();
+SeriesAlreadyExistsError.prototype.coloredMessage = function(){
+    return this.message.magenta;
+};
+
+
+SkippedSeriesError.prototype = new Error();
+SkippedSeriesError.prototype.coloredMessage = function(){
+    return this.message.red;
+};
+
 if(process.argv.length == 2){
     console.log("Need specify file name with urls");
     return;
 }
 
-var jsonDataFile = process.argv[2];
+var jsonListFile = process.argv[2];
 var pathToSaveFiles = process.argv[3];
-var canPostData = (process.argv[4] == 'post-data');
-var host = process.argv[5];
-var seriesId = process.argv[6];
+var pathToSavePosters = process.argv[4];
+var canPostData = (process.argv[5] == 'post-data');
+var host = process.argv[6];
 
 console.reset = function () {
     return process.stdout.write('\033c');
@@ -148,6 +169,43 @@ function download_using_node_wget(fileUrl, fileName, outputdir, headers, cb) {
         var currentProgress = progress * 100;
         console.reset();
         console.log('fileName: ', filePathWithName, ' progress: ', currentProgress, '%');
+    });
+}
+
+function downloadPosters(posters, threads, cb) {
+    if(posters.length == 0) {
+        cb(null, true);
+        return;
+    }
+
+    async.eachLimit(posters, threads, function (poster, next) {
+        console.log('Posters download: ', poster);
+        var fullPathToSaveFiles = poster.baseDir;
+        mkdirp(fullPathToSaveFiles, function(err) {
+            if(err){
+                throw err;
+            }
+            download_using_node_wget(poster.url,
+                null,
+                fullPathToSaveFiles, {'User-Agent': USER_AGENT},
+                function(err, data){
+                    if(err instanceof FileExistsError) {
+                        console.log('Skipped downloading! File already exists!');
+                        warningStack.push(err);
+                        next(null, data);
+                    }else{
+                        next(err, data);
+                    }
+                });
+        });
+    }, function (err) {
+        if (err) {
+            console.log('finished with error: ', err);
+            cb(err, false);
+        } else {
+            console.log('finished!');
+            cb(null, true);
+        }
     });
 }
 
@@ -277,6 +335,46 @@ function postData(host, seriesId, seasonNumber, formData, cb){
     );
 }
 
+function postSerialData(host, type, formData, cb){
+
+    if(!canPostData){
+        cb(null, null);
+        return;
+    }
+
+    var videoTypes = {
+        'shows' : 'series',
+        'movie' : 'movie'
+    };
+
+    request.post(
+        host + '/api/series/create/' + videoTypes[type],
+        {
+            form: formData
+        },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var jsonResponse = JSON.parse(body);
+
+                if(jsonResponse.success){
+                    if(!jsonResponse.created) {
+                        warningStack.push(new SeriesAlreadyExistsError(jsonResponse.id));
+                    }
+                    cb(null, jsonResponse.id);
+                }else{
+                    cb(jsonResponse.errors, null);
+                }
+            } else {
+                if(error) {
+                    cb(error, null);
+                }else if(response.statusCode != 200){
+                    cb(new Error("Status code " + response.statusCode));
+                }
+            }
+        }
+    );
+}
+
 function printWarningStack() {
     if (warningStack.length) {
         console.log('\n');
@@ -289,68 +387,131 @@ function printWarningStack() {
     }
 }
 
-fs.readFile(jsonDataFile, 'utf8', function (err, data) {
-    if (err) throw err;
-    var downloadData = JSON.parse(data);
-    var videos = [];
-    var subtitles = [];
-    var threads = 1;
 
-    for(var i=0, len=downloadData.videoInfos.length; i<len; i++) {
-        var videoInfo = downloadData.videoInfos[i];
-        var title = videoInfo.title;
-        var parsedTitle = /Season\s(.*)\sEpisode\s(.*)/.exec(title);
-        var seasonNumber = parsedTitle && parsedTitle[1] || 1;
-        var episodeNumber = parsedTitle && parsedTitle[2] || 1;
+function processFile(videoType, jsonDataFilePath, postersRootDir, cb) {
+    fs.readFile(jsonDataFilePath, 'utf8', function (err, data) {
+        if (err) throw err;
+        var downloadData = JSON.parse(data);
+        var firstVideoInfo = downloadData.videoInfos[0];
+        var releaseYear = /\d+/g.exec(downloadData.posterInfo.year)[0];
+        var countryInfo = /Countries:(.*)/g.exec('Countries: USA')[1].trim();
 
-        var seriesIdWithSeasonNumberPath = ('/' + seriesId + '/' + seasonNumber);
-        var onlySeasonNumberPath = ('/' + seasonNumber);
-        var computedFilePath = seriesId ? seriesIdWithSeasonNumberPath : onlySeasonNumberPath;
-        for(var j=0, jlen=videoInfo.sourceUrls.length; j<jlen; j++) {
-            var sourceUrl = videoInfo.sourceUrls[j];
-            var fileExtension = url.parse(sourceUrl.src).pathname.split('.').pop();
+        var seriesInfo = {
+            original_title: firstVideoInfo.title,
+            title: firstVideoInfo.title,
+            description: downloadData.description,
+            year_start: releaseYear,
+            poster: postersRootDir + '/' + path.basename(firstVideoInfo.posterUrl),
+            original_language: 'en',
+            country: countryInfo
+        };
 
-            videos.push({
-                title: title,
-                seriesId: seriesId,
-                seasonNumber: seasonNumber,
-                episodeNumber: episodeNumber,
-                filePath: computedFilePath,
-                fileName: episodeNumber+'.'+fileExtension,
-                description: downloadData.description,
-                needPostToApi: (j==0),
-                src: sourceUrl.src
-            });
-        }
-        for(var k=0, klen=videoInfo.trackUrls.length; k<klen; k++) {
-            subtitles.push({
-                filePath: computedFilePath,
-                src: videoInfo.trackUrls[k].src
-            });
-        }
-    }
+        function processDownloading(downloadData, seriesId, cb) {
+            var videos = [];
+            var subtitles = [];
+            var threads = 1;
+            var posters = [];
 
-    console.info('Videos count: ', videos.length);
-    console.info('Subtitles count: ', subtitles.length);
+            for (var i = 0, len = downloadData.videoInfos.length; i < len; i++) {
+                var videoInfo = downloadData.videoInfos[i];
+                var title = videoInfo.title;
+                var parsedTitle = /Season\s(.*)\sEpisode\s(.*)/.exec(title);
+                var seasonNumber = parsedTitle && parsedTitle[1] || 1;
+                var episodeNumber = parsedTitle && parsedTitle[2] || 1;
+                var seriesIdWithSeasonNumberPath = ('/' + seriesId + '/' + seasonNumber);
+                var onlySeasonNumberPath = ('/' + seasonNumber);
+                var computedFilePath = seriesId ? seriesIdWithSeasonNumberPath : onlySeasonNumberPath;
+                for (var j = 0, jlen = videoInfo.sourceUrls.length; j < jlen; j++) {
+                    var sourceUrl = videoInfo.sourceUrls[j];
+                    var fileExtension = url.parse(sourceUrl.src).pathname.split('.').pop();
+                    videos.push({
+                        title: title,
+                        seriesId: seriesId,
+                        seasonNumber: seasonNumber,
+                        episodeNumber: episodeNumber,
+                        filePath: computedFilePath,
+                        fileName: episodeNumber + '.' + fileExtension,
+                        description: downloadData.description,
+                        needPostToApi: (j == 0),
+                        src: sourceUrl.src
+                    });
+                }
+                for (var k = 0, klen = videoInfo.trackUrls.length; k < klen; k++) {
+                    subtitles.push({
+                        filePath: computedFilePath,
+                        src: videoInfo.trackUrls[k].src
+                    });
+                }
 
-    downloadSubtitles(subtitles, threads, function(err, success){
-
-        if(success) {
-            console.log('Subtitles downloaded successfully!');
-        }else{
-            console.error('Subtitles downloading failed!');
-        }
-
-        downloadVideos(videos, threads, function(err, success){
-            if(success) {
-                console.log('Videos downloaded successfully!');
-            }else{
-                console.error('Videos downloading failed'.red, err);
+                if(i == 0){
+                    posters.push({url: videoInfo.posterUrl,
+                                  fullPath: seriesInfo.poster,
+                                  baseDir: postersRootDir});
+                }
             }
 
-            printWarningStack();
-            process.exit();
+            console.info('Videos count: ', videos.length);
+            console.info('Subtitles count: ', subtitles.length);
+
+            downloadPosters(posters, threads, function (err, success) {
+                if (success) {
+                    console.log('Poster(s) downloaded successfully!');
+                } else {
+                    console.error('Poster(s) downloading failed!');
+                }
+                downloadSubtitles(subtitles, threads, function (err, success) {
+                    if (success) {
+                        console.log('Subtitles downloaded successfully!');
+                    } else {
+                        console.error('Subtitles downloading failed!');
+                    }
+                    downloadVideos(videos, threads, function (err, success) {
+                        if (success) {
+                            console.log('Videos downloaded successfully!');
+                        } else {
+                            console.error('Videos downloading failed'.red, err);
+                        }
+                        printWarningStack();
+                        cb(err, success);
+                    });
+                });
+            });
+        }
+
+
+        postSerialData(host, videoType, seriesInfo, function(err, seriesId){
+            if(err){
+                warningStack.push(new SkippedSeriesError(jsonDataFilePath, err))
+                cb(err, null);
+            }else {
+                processDownloading(downloadData, seriesId, cb);
+            }
         });
     });
+}
 
+
+function readAndProcessList(pathToList, posterRootDir, cb){
+    fs.readFile(pathToList, 'utf8', function (err, data) {
+        if (err) throw err;
+        var listData = JSON.parse(data);
+        async.eachLimit(listData.links, 1, function (data, next) {
+            processFile(data.type, data.file, posterRootDir, function(err, data){
+                next(err, data);
+            });
+        }, function(err){
+            if (err) {
+                console.log('Serials analyzing finished with error: ', err);
+                cb(err, false);
+            } else {
+                console.log('Serials analyzing finished!');
+                cb(null, true);
+            }
+        });
+    });
+}
+
+
+readAndProcessList(jsonListFile, pathToSavePosters, function(err, success){
+    console.log("Finished ", success ? ' successfully!' : ' with errors!');
 });
